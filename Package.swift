10d3506663d5c5d2ad86e98977b89e361732cb11
Ryan Wachowski <wachowski.1@osu.edu>
2020-09-16 11:53:38 -0400

    Updates SwiftSyntax and one code point to be compatible with Xcode 12

--- Package.swift
+++ Package.swift
@@ -2,6 +2,12 @@
 
 import PackageDescription
 
+// tag "swift-5.3-DEVELOPMENT-SNAPSHOT-2020-09-09-a" is the latest
+// version which is compatible with Swift 5.3. Actual syntax changes
+// are minimal (or even stable?) so the impact of using a dev snapshot
+// is presumably minimal.
+let swiftSyntaxRevision = "e39d380a1877da137d45cc5dcd23676a3fd00476"
+
 let package = Package(
     name: "Hexicon",
     platforms: [.iOS(.v13), .macOS(.v10_15)],
@@ -11,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "Commandant", url: "https://github.com/carthage/commandant", from: "0.17.0"),
-        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax", .exact("0.50200.0")),
+        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax", .revision(swiftSyntaxRevision)),
     ],
     targets: [
         .target(name: "Hexicon", dependencies: []),
